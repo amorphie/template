a0b9a7b006e94a27832f9c4691428c7eab0d4a04
Mehmet TOSUN <93265833+middt@users.noreply.github.com>
2023-06-15 13:21:53 +0300

    Updated to framework 1.1 Added mock.json for mock services

--- amorphie.template/Module/StudentModule.cs
+++ amorphie.template/Module/StudentModule.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using amorphie.template.core.Model;
 using amorphie.template.Validator;
+using amorphie.core.Module.minimal_api;
+using amorphie.template.data;
 
 namespace amorphie.template.Module;
 
-public sealed class StudentModule : BaseTemplateModule<Student, Student, StudentValidator>
+public sealed class StudentModule : BaseBBTRoute<Student, Student, TemplateDbContext>
 {
-    public StudentModule(WebApplication app) : base(app)
-    {
-    }
+    public StudentModule(WebApplication app)
+        : base(app) { }
 
-    public override string[]? PropertyCheckList => new string[] { "FirstMidName","LastName" };
+    public override string[]? PropertyCheckList => new string[] { "FirstMidName", "LastName" };
 
     public override string? UrlFragment => "student";
+
+
+    public override void AddRoutes(RouteGroupBuilder routeGroupBuilder)
+    {
+        base.AddRoutes(routeGroupBuilder);
+    }
+  
 }
