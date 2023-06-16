40342d31ed1d3fdb254a38013d1836bba9bf0810
Mehmet TOSUN <93265833+middt@users.noreply.github.com>
2023-06-16 14:12:10 +0300

    Added mock service Added sample for custom search

--- amorphie.template/Module/StudentModule.cs
+++ amorphie.template/Module/StudentModule.cs
@@ -6,10 +6,14 @@ using amorphie.template.core.Model;
 using amorphie.template.Validator;
 using amorphie.core.Module.minimal_api;
 using amorphie.template.data;
+using Microsoft.AspNetCore.Mvc;
+using amorphie.template.core.Search;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 namespace amorphie.template.Module;
 
-public sealed class StudentModule : BaseBBTRoute<Student, Student, TemplateDbContext>
+public class StudentModule : BaseBBTRoute<StudentDTO, Student, TemplateDbContext>
 {
     public StudentModule(WebApplication app)
         : base(app) { }
@@ -18,10 +22,29 @@ public sealed class StudentModule : BaseBBTRoute<Student, Student, TemplateDbCon
 
     public override string? UrlFragment => "student";
 
-
     public override void AddRoutes(RouteGroupBuilder routeGroupBuilder)
     {
         base.AddRoutes(routeGroupBuilder);
+
+        routeGroupBuilder.MapGet("/search", SearchMethod);
     }
-  
+
+    protected async  ValueTask<IResult> SearchMethod(
+        [FromServices] TemplateDbContext context,
+        [FromServices] IMapper mapper,
+        [AsParameters] StudentSearch userSearch
+    )
+    {
+        IList<Student> resultList = await context
+            .Set<Student>()
+            .AsNoTracking()
+            .Where(x=>x.FirstMidName.Contains(userSearch.Keyword!) || x.LastName.Contains(userSearch.Keyword!))
+            .Skip(userSearch.Page)
+            .Take(userSearch.PageSize)
+            .ToListAsync();
+
+        return (resultList != null && resultList.Count > 0)
+            ? Results.Ok(mapper.Map<IList<StudentDTO>>(resultList))
+            : Results.NoContent();
+    }
 }
