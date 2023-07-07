bf39082b2b96c0175499dfcadae239a5e5ef72d8
Mehmet TOSUN <93265833+middt@users.noreply.github.com>
2023-07-07 14:13:51 +0300

    Minor startup fix

--- amorphie.template/Program.cs
+++ amorphie.template/Program.cs
@@ -18,7 +18,6 @@ builder.Services.AddHealthChecks().AddBBTHealthCheck();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGen(options=>
 {
     options.OperationFilter<AddSwaggerParameterFilter>();
