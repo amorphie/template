78a7f1ce601227f740b5f0297556542a3ca176ee
mt <mehmet.tosun@outlook.com>
2023-07-13 16:17:54 +0300

    Changes in docker-compose

--- vault.sh
+++ vault.sh
@@ -1,2 +1,2 @@
 sleep 5 &&
-curl -X POST 'http://vault:8200/v1/secret/data/amorphie-template' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"pass": "my-password", "username":"my-username"} }'
+curl -X POST 'http://bbt-template-vault:8200/v1/secret/data/amorphie-template' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"pass": "my-password", "username":"my-username"} }'
